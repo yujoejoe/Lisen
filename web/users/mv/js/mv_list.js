a91b2900f59071d6edd1042734d24c637b63dc55
xsirl <1296080957@qq.com>
2019-11-20 08:04:44 +0800

    添加mv

---
+++ web/users/mv/js/mv_list.js
@@ -0,0 +1,16 @@
+/**
+ * Created by user on 2019/11/19.
+ */
+
+var mv_list = document.getElementById('id_mv_list');
+
+// for(var i = 0; i < 20; i ++){
+//   var li = document.createElement('li');
+//   li.setAttribute('class','mv_list_item');
+//   var a = document.createElement('a');
+//   a.setAttribute('class','item_img');
+//   li.appendChild(a);
+//   var h3 = document.createElement('h3');
+//
+//   mv_list.appendChild(li);
+// }
