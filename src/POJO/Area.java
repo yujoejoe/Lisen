883adff8f927b18a082e53e876d289020e695b33
xsirl <1296080957@qq.com>
2019-12-03 17:29:34 +0800

    添加mv.js

---
+++ src/POJO/Area.java
@@ -0,0 +1,30 @@
+package POJO;
+
+/**
+ * Created by user on 2019/12/3.
+ */
+public class Area {
+    int id;
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return '\'' + name + '\'';
+    }
+}
--- web/users/mv/js/mv.js
+++ web/users/mv/js/mv.js
@@ -9,7 +9,6 @@ document.onreadystatechange = loading;
 
 
 
-
 function MV(area, version, page){
   this.area = area;
   this.version = version;
@@ -19,69 +18,98 @@ function MV(area, version, page){
 }
 
 MV.prototype = {
+  constructor: MV,
   init: function(){
     var self = this;
 
     // 获取mv顶部标签容器
-    var tags = document.getElementById('tags');
+	var tags = document.getElementById('tags');
+	while(tags.hasChildNodes()){
+	  tags.removeChild(tags.firstChild);
+    }
+
+    console.log("tags: " + tags);
+
+    // 创建"区域"标签和"版本"标签
+    var areaTag = document.createElement('div');
+    var versionTag = document.createElement('div');
+    areaTag.className = "tag";
+    versionTag.className = "tag";
+    // 创建小标题
+    var title1 = document.createElement('h3');
+    title1.className = "tag_title";
+    title1.innerHTML = "区域";
+	var title2 = document.createElement('h3');
+	title2.className = "tag_title";
+	title2.innerHTML = "版本";
+    // 创建a标签
+	var all = document.createElement('a');
+	all.className = "tag_item";
+	all.setAttribute('href',"javascript:;");
+	all.innerHTML = "全部";
+
+	// 添加子元素
+	areaTag.appendChild(title1);
+	versionTag.appendChild(title2);
+    areaTag.appendChild(all);
+    versionTag.appendChild(all);
+
+    // console.log("areaTag" + areaTag);
+    // console.log("versionTag" + versionTag);
+
+
 
     $.ajax({
       type: "get",
       url: "/infoGet",
       data: {"area": "all", "version": "all", "page": this.page},
-      dataType: "json",
       async: false,
-      success: function(data){
+      success: function(result){
+
+        var data = JSON.parse(result);
+
+		console.log(data);
         // success:
-        // type:  二维数组
+        // area:
+        // version:
         // content: artist, title, play, date, img    对象数组
         if(data.success === false){
+		  console.log("failure");
           self.content = null;
           self.isEmpty = true;
           return;
         }
 
-        for( var i = 0; i < data.type.length; ++i){
-          // 添加类型标签
-          var tag = document.createElement('div');
-          tag.className = 'tag';
-          // 添加标题
-          var title = document.createElement('h3');
-          title.className = 'tag_title';
-
-  // 添加标题（未完成）   title.
-
-          // 添加‘全部’标签
-          var all = document.createElement('a');
-          all.classList.add('tag_item', 'tag_item_select');
-          all.innerHTML = "全部";
-
-          tag.appendChild(title);
-          tag.appendChild(all);
-
-          data.type[i].forEach(function(ele){
-            var aTag = document.createElement('a');
-            aTag.className = 'tag_item';
-            aTag.innerHTML = ele;
-            tag.appendChild(aTag);
-          });
-
-          tags.appendChild(tag);
+        for( var i = 0; i < data.area.length; ++i){
+          var aTag = document.createElement('a');
+          aTag.className = "tag_item";
+          aTag.setAttribute('href',"javascript:;");
+          aTag.innerHTML = data.area[i];
+          areaTag.appendChild(aTag);
         }
 
+		for( var i = 0; i < data.version.length; ++i){
+		  var aTag = document.createElement('a');
+		  aTag.className = "tag_item";
+		  aTag.setAttribute('href',"javascript:;");
+		  aTag.innerHTML = data.version[i];
+		  versionTag.appendChild(aTag);
+		}
+
+		// 把标签放入tags容器
+        tags.appendChild(areaTag);
+        tags.appendChild(versionTag);
 
 
-
-
-        for(var j  = 0 ; j < data.content.length; ++j){
-          self.content.push({
-            "img": data.content[i].img,
-            "date": data.content[i].date,
-            "play": data.content[i].play,
-            "title": data.content[i].title,
-            "artist": data.content[i].artist
-          });
-        }
+        // for(var j  = 0 ; j < data.content.length; ++j){
+        //   self.content.push({
+        //     "img": data.content[i].img,
+        //     "date": data.content[i].date,
+        //     "play": data.content[i].play,
+        //     "title": data.content[i].title,
+        //     "artist": data.content[i].artist
+        //   });
+        // }
       }
     });
   }
@@ -100,3 +128,11 @@ function loading(){
     load.style.display = "block";
   }
 }
+
+
+window.onload = function () {
+
+  var mv = new MV();
+  console.log(mv);
+  mv.init();
+};
