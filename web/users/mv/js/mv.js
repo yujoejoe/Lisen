bbfc4a9f3dee1a8eee68419d6d2b07658085677a
xsirl <1296080957@qq.com>
2019-12-05 20:53:07 +0800

    添加mv.js

--- web/users/mv/js/mv.js
+++ web/users/mv/js/mv.js
@@ -8,88 +8,22 @@ document.onreadystatechange = loading;
 function MV() {
   this.area = "全部";
   this.version = "全部";
-  this.page = 1;
+  this.page = 0;
   this.content = [];
+  this.counts = 1;
   this.isEmpty = true;
 }
 
 MV.prototype = {
   constructor: MV,
   init: function () {
-	var self = this;
 	var data = this.getData();
 
-	// 获取mv顶部标签容器
-	var tags = document.getElementById('tags');
-	clearChild(tags);
+	// 添加标签
+	this.reset(data);
+	this.addTag(data);
+	this.addPage(data);
 
-	// console.log("tags: " + tags);
-
-	// 创建"区域"标签和"版本"标签
-	var areaTag = document.createElement('div');
-	var versionTag = document.createElement('div');
-	areaTag.className = "tag";
-	versionTag.className = "tag";
-	// 创建小标题
-	var title1 = document.createElement('h3');
-	title1.className = "tag_title";
-	title1.innerHTML = "区域";
-	var title2 = document.createElement('h3');
-	title2.className = "tag_title";
-	title2.innerHTML = "版本";
-	// 创建a标签
-	var all1 = document.createElement('a');
-	all1.classList.add("tag_item", "selected");
-	all1.setAttribute('href', "javascript:;");
-	all1.innerHTML = "全部";
-	var all2 = document.createElement('a');
-	all2.classList.add("tag_item", "selected");
-	all2.setAttribute('href', "javascript:;");
-	all2.innerHTML = "全部";
-
-	// 添加子元素
-	areaTag.appendChild(title1);
-	versionTag.appendChild(title2);
-	areaTag.appendChild(all1);
-	versionTag.appendChild(all2);
-
-	// console.log("areaTag" + areaTag);
-	// console.log("versionTag" + versionTag);
-
-
-	if (data.success === false) {
-	  console.log("failure");
-	  self.content = null;
-	  return;
-	}
-
-	if (self.isEmpty) {
-	  for (var i = 0; i < data.area.length; ++i) {
-		var aTag = document.createElement('a');
-		aTag.className = "tag_item";
-		aTag.setAttribute('href', "javascript:;");
-		aTag.innerHTML = data.area[i];
-		areaTag.appendChild(aTag);
-	  }
-
-	  for (var i = 0; i < data.version.length; ++i) {
-		var aTag = document.createElement('a');
-		aTag.className = "tag_item";
-		aTag.setAttribute('href', "javascript:;");
-		aTag.innerHTML = data.version[i];
-		versionTag.appendChild(aTag);
-	  }
-	  // 把标签放入tags容器
-	  tags.appendChild(areaTag);
-	  tags.appendChild(versionTag);
-	  self.isEmpty = false;
-	}
-
-	var page = document.createElement('div');
-	page.className = "page";
-
-
-	self.resetList(data);
 
   },
   appendTo: function (pos) {
@@ -108,6 +42,7 @@ MV.prototype = {
 	  //1.1.1、 创建图片容器
 	  var itemBox = document.createElement('a');
 	  itemBox.className = "item_box";
+	  itemBox.setAttribute('href',"./mv_play.html");
 	  //1.1.1.1、 创建图片
 	  var itemImg = document.createElement('img');
 	  itemImg.className = "item_img";
@@ -157,13 +92,21 @@ MV.prototype = {
 	  pos.appendChild(liItem);
 	}
   },
-  resetList: function (data) {
-    console.log(data);
+  reset: function (data) {
+	console.log(data);
+
+	if(data.success === false){
+	  return;
+	}
+	this.counts = data.counts;
 
 	if (data.result.length === 0) {
 	  return;
 	}
-	for (var j = 0; j < data.result.length; ++j) {
+
+	var size = data.result.length > 12 ? 12 : data.result.length;
+
+	for (var j = 0; j < size; ++j) {
 	  this.content.push({
 		"img": data.result[j].img,
 		"date": data.result[j].date,
@@ -174,21 +117,108 @@ MV.prototype = {
 	  });
 	}
   },
-  getData: function(){
-    var data;
+  getData: function () {
+	var data;
 	$.ajax({
 	  type: "get",
 	  url: "/infoGet",
 	  data: {"area": this.area, "version": this.version, "page": this.page},
 	  async: false,
 	  success: function (result) {
-
 		// console.log(result);
 
 		data = JSON.parse(result);
 	  }
 	});
 	return data;
+  },
+  addTag: function (data) {
+	// 获取mv顶部标签容器
+	var tags = document.getElementById('tags');
+	clearChild(tags);
+
+	// console.log("tags: " + tags);
+
+	// 创建"区域"标签和"版本"标签
+	var areaTag = document.createElement('div');
+	var versionTag = document.createElement('div');
+	areaTag.className = "tag";
+	versionTag.className = "tag";
+	// 创建小标题
+	var title1 = document.createElement('h3');
+	title1.className = "tag_title";
+	title1.innerHTML = "区域";
+	var title2 = document.createElement('h3');
+	title2.className = "tag_title";
+	title2.innerHTML = "版本";
+	// 创建a标签
+	var all1 = document.createElement('a');
+	all1.classList.add("tag_item", "selected");
+	all1.setAttribute('href', "javascript:;");
+	all1.innerHTML = "全部";
+	var all2 = document.createElement('a');
+	all2.classList.add("tag_item", "selected");
+	all2.setAttribute('href', "javascript:;");
+	all2.innerHTML = "全部";
+
+	// 添加子元素
+	areaTag.appendChild(title1);
+	versionTag.appendChild(title2);
+	areaTag.appendChild(all1);
+	versionTag.appendChild(all2);
+
+	// console.log("areaTag" + areaTag);
+	// console.log("versionTag" + versionTag);
+
+
+	if (this.isEmpty) {
+	  for (var i = 0; i < data.area.length; ++i) {
+		var aTag = document.createElement('a');
+		aTag.className = "tag_item";
+		aTag.setAttribute('href', "javascript:;");
+		aTag.innerHTML = data.area[i];
+		areaTag.appendChild(aTag);
+	  }
+
+	  for (var i = 0; i < data.version.length; ++i) {
+		var aTag = document.createElement('a');
+		aTag.className = "tag_item";
+		aTag.setAttribute('href', "javascript:;");
+		aTag.innerHTML = data.version[i];
+		versionTag.appendChild(aTag);
+	  }
+	  // 把标签放入tags容器
+	  tags.appendChild(areaTag);
+	  tags.appendChild(versionTag);
+	  this.isEmpty = false;
+	}
+  },
+  addPage: function(data){
+	var main = document.getElementsByClassName('main')[0];
+	console.log(main);
+	var pageBox = document.createElement('div');
+	pageBox.className = "page";
+	var size = parseInt(this.counts / 12);
+	console.log(size);
+	if(size <= 1){
+
+	} else if(size < 4 ) {
+	  for (var i = 0; i < size; ++i) {
+		var page = "<a href=\"javascript:;\" class=\"page_index";
+		if(i === 0) {
+		  page += " selected\">" + i + 1 + "</a>";
+		}
+		else{
+		  page += "\">" + i + 1 + "</a>";
+		}
+	  }
+	  pageBox.innerHTML = page;
+	  main.appendChild(pageBox);
+	}
+
+  },
+  clear: function(){
+    this.content.length = 0;
   }
 };
 
@@ -213,9 +243,6 @@ window.onload = function () {
   var mvList = document.getElementById('mv_list');
   mv.appendTo(mvList);
 
-  var area = "全部";
-  var version = "全部";
-  var page = 1;
 
   var areaTags = document.getElementById('tags').firstChild.children;
   var versionTags = document.getElementById('tags').lastChild.children;
@@ -225,46 +252,33 @@ window.onload = function () {
 
 
 
-  function getData(area, version, page) {
-	$.ajax({
-	  type: "get",
-	  url: "/infoGet",
-	  data: {"area": area, "version": version, "page": page},
-	  success: function (result) {
-		var data = JSON.parse(result);
-		if (data.success === false) {
-		  console.log("failure");
-		  mv.content.length = 0;
-		}else{
-		  mv.resetList(data);
-		}
-	  }
-	});
-  }
 
   function bindEvent(ele, type) {
 	var index = 1;
-	for(var i = 1; i < ele.length; ++i){
+	for (var i = 1; i < ele.length; ++i) {
 	  ele[i].id = i;
-	  ele[i].onclick = function(){
+	  ele[i].onclick = function () {
 		ele[index].classList.remove("selected");
 		this.classList.add("selected");
 		index = this.id;
 
-		if(type === "area"){
-		  area = this.innerText;
-		}else if(type === "version"){
-		  version = this.innerText;
-		}else if(type === "page"){
-		  page = this.innerText;
+		if (type === "area") {
+		  mv.area = this.innerText;
+		} else if (type === "version") {
+		  mv.version = this.innerText;
+		} else if (type === "page") {
+		  mv.page = this.innerText;
 		}
-
-		getData(area, version, page);
+		// 1、先清除mv的内容
+		mv.clear();
+		// 2、重新获取数据重置mv的内容
+		mv.reset(mv.getData());
+		// 3、把mv的内容添加到容器
+		mv.appendTo(mvList);
 
 	  }
 	}
   }
-
 };
 
 function clearChild(parent) {
